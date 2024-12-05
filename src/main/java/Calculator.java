f52cdd7f3df845f82a38b16bf57be3ef5421e818
MatveyJI <lis_yra@mail.ru>
2024-12-05 09:15:49 +0300

    make calculator with my method and fix readme

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,17 +1,34 @@
 public class Calculator {
-    public int add(int a, int b){
-        //TODO inser your realisation in method add
+    public int add(int a, int b) {
+        return a + b;
     }
-    public int dif(int a, int b){
-        //TODO inser your realisation in method dif
+
+    public int dif(int a, int b) {
+        return a - b;
     }
-    public int div(int a, int b){
-        //TODO inser your realisation in method div
+
+    public int div(int a, int b) {
+        if (b == 0) {
+            throw new ArithmeticException("Деление на ноль запрещено");
+        }
+        return a / b;
     }
-    public int times(int a, int b){
-        //TODO inser your realisation in method times
+
+    public int times(int a, int b) {
+        return a * b;
     }
-    public int solver(){
-        //TODO inser your realisation in method solver
+
+    public int solver() {
+        int part1 = add(10, 5);
+        int part2 = times(part1, 2);
+        int part3 = div(20, 4);
+        int result = dif(part2, part3);
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        Calculator calculator = new Calculator();
+        System.out.println("Result of solver: " + calculator.solver());
     }
 }
